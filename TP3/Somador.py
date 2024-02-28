c4626e09526b33a9ffe2c9120fb90550e72e49bb
jvale03 <carlosvalejcov@gmail.com>
2024-02-28 19:52:49 +0000

    semana 3 concluida

---
+++ TP3/Somador.py
@@ -0,0 +1,28 @@
+import re
+import sys
+
+def somador(file):
+    lines = file.readlines()
+    line = "".join(lines)
+    status = False
+    contador = 0
+
+    patterns = re.findall(r'(off|on|=|\d+)',line,re.IGNORECASE)
+    for pattern in patterns:
+        if re.search(r'off',pattern,re.IGNORECASE):
+            status = False
+        elif re.match(r'on',pattern,re.IGNORECASE):
+            status = True
+        elif re.match(r'\d+',pattern):
+            if status == True:
+                contador += int(pattern)
+        elif re.match(r'=',pattern):
+            print(contador)
+    
+
+if __name__ == "__main__":
+    file = open(sys.argv[1],"r")
+    
+    somador(file)
+
+    file.close()
